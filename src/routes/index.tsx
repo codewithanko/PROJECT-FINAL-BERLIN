8a36e428dcf9f66d8c7e1230c348ae27cf1ff33f
KAJUMBA KELLY <ankotrip1@gmail.com>
2026-07-19 22:17:06 +0300

    fix: remove Sentry initialization to resolve 500 build error

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,5 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  dsn: "https://77d787875b6ef2bbfb09530f8daf656b@o4511763169935360.ingest.us.sentry.io/4511763181469696",
-  dataCollection: {
-    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
-    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#dataCollection
-    // userInfo: false,
-    // httpBodies: []
-  }
-});
-
-const container = document.getElementById("app");
-const root = createRoot(container);
-root.render(<App />);
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
